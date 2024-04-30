79a88a586fe12432648faa9a2f4072100e57def2
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2024-04-30 22:13:18 +0300

    add markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Description from "./Description/Description";
+import Options from "./Options/Options";
+import Feedback from "./Feedback/Feedback";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  //  const [isVisible, setIsVisible] = useState(false);
+
+  //  const handleToggle = () => {
+  //    setIsVisible(!isVisible);
+  //  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Description />
+      <Options />
+      {/* <button onClick={handleToggle}>{isVisible ? "Hide" : "Show"}</button>
+      {isVisible && ()} */}
+      <Feedback feedback={feedback} />
+    </div>
+  );
+};
+export default App;
 
-export default App
+// const App = () => {
+//   const [values, setValues] = useState({
+//   good: 0,
+// 	neutral: 0,
+// 	bad: 0
+//   });
+
+//   const updateX = () => {
+//     setValues({
+//       ...values,
+//       x: values.x + 1,
+//     });
+//   };
+
+//   const updateY = () => {
+//     setValues({
+//       ...values,
+//       y: values.y + 1,
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <p>
+//         x: {values.x}, y: {values.y}
+//       </p>
+
+//       <button onClick={updateX}>Update x</button>
+//       <button onClick={updateY}>Update y</button>
+//     </div>
+//   );
+// };
